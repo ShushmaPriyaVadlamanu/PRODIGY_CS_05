94af7da4a329e533350acd2413ca85ab18262988
ShushmaPriyaVadlamanu <149227381+ShushmaPriyaVadlamanu@users.noreply.github.com>
2024-07-01 12:03:37 +0530

    output.py

---
+++ output.py
@@ -0,0 +1,82 @@
+C:\Users\Sushma\PycharmProjects\PRODIGY_CS_05\.venv\Scripts\python.exe C:\Users\Sushma\PycharmProjects\PRODIGY_CS_05\packet_analyzer.py 
+Starting Network Packet Analyzer...
+Source IP: 10.56.27.33, Destination IP: 224.0.0.252, Protocol: 17
+Payload: UDP 10.56.27.33:57487 > 224.0.0.252:llmnr / LLMNRQuery who has 'wpad.'
+Source IP: 10.56.27.33, Destination IP: 224.0.0.252, Protocol: 17
+Payload: UDP 10.56.27.33:52903 > 224.0.0.252:llmnr / LLMNRQuery who has 'wpad.'
+Source IP: 10.56.27.33, Destination IP: 10.56.31.255, Protocol: 17
+Payload: UDP / NBNSHeader / NBNSQueryRequest who has '\\WPAD'
+Source IP: 10.56.27.33, Destination IP: 10.56.31.255, Protocol: 17
+Payload: UDP / NBNSHeader / NBNSQueryRequest who has '\\WPAD'
+Source IP: 10.56.3.248, Destination IP: 224.0.0.251, Protocol: 17
+Payload: UDP / DNS Qry "b'HarshaVardhan\xe2\x80\x99s MacBook Air._companion-link._tcp.local.'" 
+Source IP: 10.56.8.58, Destination IP: 224.0.0.251, Protocol: 17
+Payload: UDP / DNS Qry "b'LAPTOP-QT9GCVVF.local.'" 
+Source IP: 10.56.8.58, Destination IP: 224.0.0.251, Protocol: 17
+Payload: UDP / DNS Qry "b'LAPTOP-QT9GCVVF.local.'" 
+Source IP: 10.56.8.58, Destination IP: 224.0.0.252, Protocol: 17
+Payload: UDP 10.56.8.58:57269 > 224.0.0.252:llmnr / LLMNRQuery who has 'LAPTOP-QT9GCVVF.'
+Source IP: 10.56.19.148, Destination IP: 224.0.0.251, Protocol: 17
+Payload: UDP / DNS Qry "b'Android-4.local.'" 
+Source IP: 10.56.19.170, Destination IP: 224.0.0.251, Protocol: 17
+Payload: UDP / DNS Qry "b'LAPTOP-QFRKKHLA.local.'" 
+Source IP: 10.56.19.170, Destination IP: 224.0.0.251, Protocol: 17
+Payload: UDP / DNS Qry "b'LAPTOP-QFRKKHLA.local.'" 
+Source IP: 10.56.27.56, Destination IP: 224.0.0.251, Protocol: 17
+Payload: UDP / DNS Qry "b'_microsoft_mcc._tcp.local.'" 
+Source IP: 10.56.19.170, Destination IP: 224.0.0.251, Protocol: 17
+Payload: UDP / DNS Qry "b'LAPTOP-QT9GCVVF.local.'" 
+Source IP: 10.56.19.170, Destination IP: 224.0.0.251, Protocol: 17
+Payload: UDP / DNS Qry "b'LAPTOP-QT9GCVVF.local.'" 
+Source IP: 10.56.8.228, Destination IP: 224.0.0.251, Protocol: 17
+Payload: UDP / DNS Qry "b'Maley\xe2\x80\x99s MacBook Air._companion-link._tcp.local.'" 
+Source IP: 10.56.6.183, Destination IP: 224.0.0.251, Protocol: 17
+Payload: UDP / DNS Ans "10.56.6.183" 
+Source IP: 10.56.6.95, Destination IP: 10.56.31.255, Protocol: 17
+Payload: UDP / NBNSHeader / NBNSQueryRequest who has '\\DESKTOP-S1DSNJ9'
+Source IP: 10.56.19.170, Destination IP: 224.0.0.251, Protocol: 17
+Payload: UDP / DNS Qry "b'Android-5.local.'" 
+Source IP: 10.56.19.170, Destination IP: 224.0.0.251, Protocol: 17
+Payload: UDP / DNS Qry "b'_kerberos.Ailuris-MacBook-Air.local.'" 
+Source IP: 10.56.19.170, Destination IP: 224.0.0.251, Protocol: 17
+Payload: UDP / DNS Qry "b'Android-4.local.'" 
+Source IP: 10.56.4.183, Destination IP: 224.0.0.251, Protocol: 17
+Payload: UDP / DNS Ans "10.56.4.183" 
+Source IP: 10.56.9.111, Destination IP: 224.0.0.251, Protocol: 17
+Payload: UDP / DNS Qry "b'Android-9.local.'" 
+Source IP: 10.56.18.132, Destination IP: 10.56.31.255, Protocol: 17
+Payload: UDP / NBNSHeader / NBNSQueryRequest who has '\\SUDHAKAR'
+Source IP: 10.56.27.185, Destination IP: 224.0.0.251, Protocol: 17
+Payload: UDP / DNS Ans 
+Source IP: 10.56.3.144, Destination IP: 10.56.31.255, Protocol: 17
+Payload: UDP 10.56.3.144:2008 > 10.56.31.255:2008 / Raw
+Source IP: 10.56.3.144, Destination IP: 10.56.31.255, Protocol: 17
+Payload: UDP 10.56.3.144:2007 > 10.56.31.255:2007 / Raw
+Source IP: 10.56.28.146, Destination IP: 224.0.0.251, Protocol: 17
+Payload: UDP / DNS Qry "b'_googlecast._tcp.local.'" 
+Source IP: 10.56.26.45, Destination IP: 63.141.48.149, Protocol: 17
+Payload: UDP 10.56.26.45:ipsec_msft > 63.141.48.149:ipsec_msft / ESP
+Source IP: 63.141.48.149, Destination IP: 10.56.26.45, Protocol: 17
+Payload: UDP 63.141.48.149:ipsec_msft > 10.56.26.45:ipsec_msft / ESP
+Source IP: 63.141.48.149, Destination IP: 10.56.26.45, Protocol: 17
+Payload: UDP 63.141.48.149:ipsec_msft > 10.56.26.45:ipsec_msft / ESP
+Source IP: 10.56.26.45, Destination IP: 63.141.48.149, Protocol: 17
+Payload: UDP 10.56.26.45:ipsec_msft > 63.141.48.149:ipsec_msft / ESP
+Source IP: 10.56.1.136, Destination IP: 255.255.255.255, Protocol: 17
+Payload: UDP 10.56.1.136:37607 > 255.255.255.255:29810 / Raw
+Source IP: 10.56.28.146, Destination IP: 224.0.0.251, Protocol: 17
+Payload: UDP / DNS Qry "b'_googlecast._tcp.local.'" 
+Source IP: 10.56.19.148, Destination IP: 224.0.0.251, Protocol: 17
+Payload: UDP / DNS Qry "b'Android-5.local.'" 
+Source IP: 10.56.6.169, Destination IP: 224.0.0.251, Protocol: 17
+Payload: UDP / DNS Qry "b'_companion-link._tcp.local.'" 
+Source IP: 10.56.4.117, Destination IP: 224.0.0.251, Protocol: 17
+Payload: UDP / DNS Ans 
+Source IP: 10.56.19.170, Destination IP: 224.0.0.251, Protocol: 17
+Payload: UDP / DNS Qry "b'0._teamviewer._tcp.local.'" 
+Source IP: 10.56.19.170, Destination IP: 224.0.0.251, Protocol: 17
+Payload: UDP / DNS Qry "b'Nitesh\xe2\x80\x99s MacBook Pro._companion-link._tcp.local.'" 
+Source IP: 10.56.27.78, Destination IP: 224.0.0.251, Protocol: 17
+Payload: UDP / DNS Qry "b'Android-9.local.'" 
+Source IP: 10.56.19.170, Destination IP: 224.0.0.251, Protocol: 17
+Payload: UDP / DNS Qry "b'552d54d8-e877-48f0-86ae-8644f97062e4.local.'" 
